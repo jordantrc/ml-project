d217ca886ba8604656368f3830b5c1c1dccf586f
Jordan Chadwick <jordan@wickwick.org>
2017-03-29 20:49:44 -0400

    data_cleanup.py working for VAIS

--- code/data_cleanup.py
+++ code/data_cleanup.py
@@ -8,19 +8,30 @@
 # project.
 #
 
+import os
+
 
 def main():
     """main function"""
     # paths to datasets are relative to this script
     images_folder = "../images"
-    vais_source_folder = images_folder + "/VAIS-original"
+    destination_folder = images_folder + "/combined"
+    vais_source_folder = images_folder + "/VAIS"
 
-    cleanup_vais(vais_source_folder)
+    # cleanup VAIS dataset
+    cleanup_vais(vais_source_folder, destination_folder)
 
 
-def cleanup_vais(folder):
+def cleanup_vais(source_folder, dest_folder):
     """cleans up the VAIS dataset"""
-
+    index = 0
+    for root, dirs, files in os.walk(source_folder):
+        for file in files:
+            if ".png" in file and "-eo_" in file:
+                new_name = "ship_vais_" + str(index) + ".png"
+                os.rename(root + "/" + file, dest_folder + "/" + new_name)
+                print("moved " + root + "/" + file + " to " + dest_folder + "/" + new_name)
+                index += 1
 
 
 if __name__ == "__main__":
