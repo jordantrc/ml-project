1eaf6657ecf8fe8d41b227c356ad7086b7740735
Jordan Chadwick <jordan@wickwick.org>
2017-04-03 15:56:28 -0400

    imagenet cleanup

--- code/data_cleanup.py
+++ code/data_cleanup.py
@@ -17,9 +17,11 @@ def main():
     images_folder = "../images"
     destination_folder = images_folder + "/combined"
     vais_source_folder = images_folder + "/VAIS"
+    imagenet_source_folder = images_folder + "/imagenet"
 
-    # cleanup VAIS dataset
-    cleanup_vais(vais_source_folder, destination_folder)
+    # cleanup datasets
+    # cleanup_vais(vais_source_folder, destination_folder)
+    cleanup_imagenet(imagenet_source_folder, destination_folder)
 
 
 def cleanup_vais(source_folder, dest_folder):
@@ -32,6 +34,20 @@ def cleanup_vais(source_folder, dest_folder):
                 os.rename(root + "/" + file, dest_folder + "/" + new_name)
                 print("moved " + root + "/" + file + " to " + dest_folder + "/" + new_name)
                 index += 1
+
+
+def cleanup_imagenet(source_folder, dest_folder):
+    """cleans up the imagenet images"""
+    index = 0
+    for root, dirs, files in os.walk(source_folder):
+        path_parts = root.split("/")
+        label = path_parts[-1]
+        for file in files:
+            extension = file.split(".")[-1]
+            new_name = label + "_imagenet_" + str(index) + "." + extension
+            os.rename(root + "/" + file, dest_folder + "/" + new_name)
+            print("moved %s/%s to %s/%s" % (root, file, dest_folder, new_name))
+            index += 1
 
 
 if __name__ == "__main__":
