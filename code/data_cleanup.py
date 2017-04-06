d61ed6e9e37f430b6fdeacd7b58ed048590adfd4
Jordan Chadwick <jchadwick@ccom.unh.edu>
2017-04-06 15:42:13 -0400

    Made data_cleanup cross-platform. A lot of work on KNN in the project ipynb

--- code/data_cleanup.py
+++ code/data_cleanup.py
@@ -19,8 +19,12 @@ def main():
     vais_source_folder = images_folder + "/VAIS"
     imagenet_source_folder = images_folder + "/imagenet"
 
+    # create the destination folder if it doesn't exist
+    if not os.path.exists(destination_folder):
+        os.mkdir(destination_folder)    
+
     # cleanup datasets
-    # cleanup_vais(vais_source_folder, destination_folder)
+    cleanup_vais(vais_source_folder, destination_folder)
     cleanup_imagenet(imagenet_source_folder, destination_folder)
 
 
@@ -31,8 +35,10 @@ def cleanup_vais(source_folder, dest_folder):
         for file in files:
             if ".png" in file and "-eo_" in file:
                 new_name = "ship_vais_" + str(index) + ".png"
-                os.rename(root + "/" + file, dest_folder + "/" + new_name)
-                print("moved " + root + "/" + file + " to " + dest_folder + "/" + new_name)
+                new_path = os.path.join(dest_folder, new_name)
+                old_path = os.path.join(root, file)
+                os.rename(old_path, new_path)
+                print("moved " + old_path + " to " + new_path)
                 index += 1
 
 
@@ -40,14 +46,19 @@ def cleanup_imagenet(source_folder, dest_folder):
     """cleans up the imagenet images"""
     index = 0
     for root, dirs, files in os.walk(source_folder):
-        path_parts = root.split("/")
-        label = path_parts[-1]
+        path_parts = os.path.split(root)
+        label = path_parts[1]
+        print("label = " + label)
         for file in files:
             extension = file.split(".")[-1]
-            new_name = label + "_imagenet_" + str(index) + "." + extension
-            os.rename(root + "/" + file, dest_folder + "/" + new_name)
-            print("moved %s/%s to %s/%s" % (root, file, dest_folder, new_name))
-            index += 1
+            extension = extension.lower()
+            if "jpg" in extension or "jpeg" in extension:
+                new_name = label + "_imagenet_" + str(index) + "." + extension
+                new_path = os.path.join(dest_folder, new_name)
+                old_path = os.path.join(root, file)
+                os.rename(old_path, new_path)
+                print("moved %s to %s" % (old_path, new_path))
+                index += 1
 
 
 if __name__ == "__main__":
